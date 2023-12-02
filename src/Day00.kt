540bf9a2c0ec764af6b41bb3a9c11858441058cd
Pedro <paraoz22@gmail.com>
2023-12-02 11:20:35 -0300

    day01

---
+++ src/Day00.kt
@@ -0,0 +1,15 @@
+fun main() {
+  fun part1(input: List<String>): Any {
+    return input.size
+  }
+
+  fun part2(input: List<String>): Any {
+    return input.size
+  }
+
+  val input = readInput("day0")
+  val testInput = readInput("day0_test")
+
+  part1(testInput).println()
+  part2(testInput).println()
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    return input
+      .map { s -> s.filter { it.isDigit() } }
+      .map { joinFirstAndLastDigit(it) }
+      .sumOf { it.toInt() }}
+
+  fun part2(input: List<String>): Int {
+    return input
+      .map { replaceWordsWithNumbers(it) }
+      .map { s -> s.filter { it.isDigit() } }
+      .map { joinFirstAndLastDigit(it) }
+      .sumOf { it.toInt() }
+  }
+
+  val input = readInput("day01")
+  val testInput = readInput("day01_test")
+
+  part1(input).println()
+  part2(input).println()
+}
+
+
+private fun replaceWordsWithNumbers(s: String): String {
+  val dict = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+  )
+  val keys = dict.keys.toList()
+
+  var ans = ""
+  for (c in s.toCharArray()) {
+    ans += c.toString()
+    keys.forEach {
+      // add last letter so if there are two concatenated numbers the second one can also be replaced
+      val lastLetter = it.toCharArray().last().toString()
+      ans = ans.replace(it, dict[it]!! + lastLetter)
     }
+  }
+  return ans
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+private fun joinFirstAndLastDigit(it: String): String {
+  val c = it.toCharArray()
+  return c.first().toString() + c.last().toString()
 }
